971ce9105f51eddb543135aba4d0f8df9e58aa8a
emcp <emcp@whichdegree.co>
2018-06-03 20:50:51 +0200

    Adding some toasty goodness

--- index.js
+++ index.js
@@ -2,13 +2,16 @@ process.env.NODE_ENV="development";
 
 require('mahrio').runServer(process.env, __dirname)
  .then( function(server) {
- 
+
  	server.route({
 		path: '/',
 		method: 'GET',
 		handler: function(req, rep){
-			rep('Your static site here!');
+        	static_response = "<iframe src=\"https://player.twitch.tv/?channel=fireplacetv\" height=\"480\" ";
+      		static_response += "width=\"680\" frameborder=\"0\" scrolling=\"no\" allowfullscreen=\"yes\"></iframe>";
+
+			rep(static_response);
 		}
 	});
- 
+
  });
